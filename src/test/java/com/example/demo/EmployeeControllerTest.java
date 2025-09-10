c5283ecfaa88f7f96720d716a7509db61cddcb36
Lanest <lanest.lin@oocl.com>
2025-09-10 10:47:28 +0800

    refactor: use createJohnSmith and createJaneDoe in should_return_male_employee_when_employee_found

--- src/test/java/com/example/demo/EmployeeControllerTest.java
+++ src/test/java/com/example/demo/EmployeeControllerTest.java
@@ -79,21 +79,21 @@ public class EmployeeControllerTest {
                 .andExpect(jsonPath("$.gender").value("FEMALE"))
                 .andExpect(jsonPath("$.salary").value(60000.0));
     }
-//
-//    @Test
-//    void should_return_male_employee_when_employee_found() throws Exception {
-//        Employee expect = employeeController.createEmployee(johnSmith());
-//        employeeController.createEmployee(janeDoe());
-//
-//        mockMvc.perform(get("/employees?gender=male")
-//                        .contentType(MediaType.APPLICATION_JSON))
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("$[0].id").value(expect.getId()))
-//                .andExpect(jsonPath("$[0].name").value(expect.getName()))
-//                .andExpect(jsonPath("$[0].age").value(expect.getAge()))
-//                .andExpect(jsonPath("$[0].gender").value(expect.getGender()))
-//                .andExpect(jsonPath("$[0].salary").value(expect.getSalary()));
-//    }
+
+    @Test
+    void should_return_male_employee_when_employee_found() throws Exception {
+        createJohnSmith();
+        createJaneDoe();
+
+        mockMvc.perform(get("/employees?gender=male")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].id").value(2))
+                .andExpect(jsonPath("$[0].name").value("Jane Doe"))
+                .andExpect(jsonPath("$[0].age").value(28))
+                .andExpect(jsonPath("$[0].gender").value("MALE"))
+                .andExpect(jsonPath("$[0].salary").value(60000.0));
+    }
 //
 //    @Test
 //    void should_create_employee() throws Exception {
